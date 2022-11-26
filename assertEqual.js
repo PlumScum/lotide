a3928bc26748efd895a1f28d03561f35c763d9aa
Jelani Sanders <jelanisands@gmail.com>
2022-11-26 14:48:52 -0500

    improved assertEqual with template literals/template strings!

--- assertEqual.js
+++ assertEqual.js
@@ -12,9 +12,9 @@
 
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    console.log("🛑🛑🛑 Assertion Failed, actual !== expected");
+    console.log(`🛑🛑🛑 Assertion Failed, ${actual} !== ${expected}`);
   } else {
-    console.log("✔️✔️✔️ Assertion Passed, actual === expected");
+    console.log(`✔️✔️✔️ Assertion Passed, ${actual} === ${expected}`);
   }
 };
 
