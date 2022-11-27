62be72459f7e749b56791e76565fe4623ef3b1a4
Jelani Sanders <jelanisands@gmail.com>
2022-11-26 23:33:50 -0500

    Initial commit

---
+++ head.js
@@ -0,0 +1,21 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed, ${actual} !== ${expected}`);
+  } else {
+    console.log(`✔️✔️✔️ Assertion Passed, ${actual} === ${expected}`);
+  }
+};
+
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+const head = function(array) {
+  return array[0];
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]));
+// console.log(head([1, 2, 3]));
