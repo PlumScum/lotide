d86c4c85cc98d13b8ff8d379a916aab8681af8a1
Jelani Sanders <jelanisands@gmail.com>
2022-12-05 01:45:57 -0500

    Initial commit

--- head.js
+++ head.js
@@ -15,7 +15,7 @@ const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([5, 6, 7]), 5);
+assertEqual(head([5, 6, 7]), 9);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([]));
 // console.log(head([1, 2, 3]));
