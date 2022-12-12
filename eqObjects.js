f8ee5f56d41148f77358b4272e109becf9deb6aa
Jelani Sanders <jelanisands@gmail.com>
2022-12-11 23:35:54 -0500

    Step one: Function Definition/Initial Commit

---
+++ eqObjects.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed, ${actual} !== ${expected}`);
+  } else {
+    console.log(`✔️✔️✔️ Assertion Passed, ${actual} === ${expected}`);
+  }
+};
+
+
+const eqObjects = function(onject1, object2) {
+  
+};
