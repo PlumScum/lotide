04ebabff0431255988a44959766eec115357f51a
Jelani Sanders <jelanisands@gmail.com>
2022-12-11 23:58:09 -0500

    Completed steps two through four

--- eqObjects.js
+++ eqObjects.js
@@ -7,6 +7,35 @@ const assertEqual = function(actual, expected) {
 };
 
 
-const eqObjects = function(onject1, object2) {
-  
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
+
+
+const eqObjects = function(object1, object2) {
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+  if (object1Keys.length !== object2Keys.length) {
+    return false;
+  }
+  for (const key of object1Keys) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      return eqArrays(object1[key], object2[key]);
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+// const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+// console.log(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject));
