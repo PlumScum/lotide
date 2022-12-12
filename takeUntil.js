f6bb7071c5ade1ae9db896c58017a23667b2b562
Jelani Sanders <jelanisands@gmail.com>
2022-12-12 03:44:49 -0500

    Initial commit

---
+++ takeUntil.js
@@ -0,0 +1,16 @@
+const takeUntil = function(array, callback) {
+  const map1 = array.map(callback);
+  let trueLocation = map1.indexOf(true);
+  array.splice(trueLocation, array.length);
+  return array;
+};
+
+// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+// const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
+
+
+// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+// const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);
+
