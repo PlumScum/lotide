4f8d40c519b0d3f87f98095bd332b756a179f033
Jelani Sanders <jelanisands@gmail.com>
2023-01-05 02:58:00 -0500

    refactoring for testing purposes

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,12 @@
-const assertEqual = require('..assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-assertEqual(tail([5, 6, 7]), 7);
+describe("#tail", () => {
+  it("returns 3 for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), 3);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(tail(['5']), '5');
+  });
+});
