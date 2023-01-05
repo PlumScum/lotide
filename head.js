3062aa813f9483688f52866346dc1fd02d2a4418
Jelani Sanders <jelanisands@gmail.com>
2023-01-04 21:41:52 -0500

    Refactored head.js. The old code was a headach to look at x.x

--- head.js
+++ head.js
@@ -1,21 +1,8 @@
-
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed, ${actual} !== ${expected}`);
-  } else {
-    console.log(`✔️✔️✔️ Assertion Passed, ${actual} === ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-
-const head = function(array) {
+function head(array) {
   return array[0];
-};
+}
 
-assertEqual(head([5, 6, 7]), 9);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]));
-// console.log(head([1, 2, 3]));
+module.exports = head;
