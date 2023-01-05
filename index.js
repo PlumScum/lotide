76dc3f3a5decc225ae3359af321ab3142d568674
Jelani Sanders <jelanisands@gmail.com>
2023-01-05 03:21:07 -0500

    Refactored with ES6 syntax

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const head   = require('./head');
-const tail   = require('./tail');
-const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual       = require('./assertEqual');
+const eqArrays          = require('./eqArrays');
+const head              = require('./head');
+const tail              = require('./tail');
+const middle            = require('./middle');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+  assertArraysEqual,
+  assertEqual,
+  eqArrays,
+  head,
+  tail,
+  middle,
 };
