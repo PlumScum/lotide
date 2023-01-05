fdce3f8d2733239eee172baa1795783e7bcebce2
Jelani Sanders <jelanisands@gmail.com>
2023-01-05 02:55:30 -0500

    refactoring for testing purposes

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,4 +1,23 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+// const eqArrays = require('../eqArrays');
+
+// assertEqual(eqArrays(eqArrays([1, 2, 3], [1, 2, 3],)), true);
+
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays(eqArrays([1, 2, 3], [1, 2, 3],)), true);
+
+describe("#eqArrays", () => {
+  it("returns false when the array lengths aren't the same", () => {
+    assert.strictEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false);
+  });
+
+  it("returns false when the array lengths ARE the same, but don't match", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+
+  it("returns true when the arrays match", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+});
+
