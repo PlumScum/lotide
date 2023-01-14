261f6669f542cd93e496423f243eb40a9d2849a9
Jelani Sanders <77513268+PlumScum@users.noreply.github.com>
2023-01-14 03:23:40 -0500

    Refactored, added pseudocode for proper execution, added data validation, cleaned up placeholders

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,12 @@
-const assertEqual = require('./assertEqual');
-
+//compare lengths of two arrays and returns true or false
 const eqArrays = function(array1, array2) {
+  // If a parameter is null, blank, return a message and exit the function
+  if (!array1 || !array2) {
+    console.log("Error: One or more arrays are invalid.");
+    return false;
+  }
+  
+  //compare lengths of two arrays and returns true or false
   if (array1.length !== array2.length) {
     return false;
   }
@@ -11,5 +17,7 @@ const eqArrays = function(array1, array2) {
   }
   return true;
 };
+
+// console.log(eqArrays([1, 2, 3], [1, 2, 4]));
 
 module.exports = eqArrays;
