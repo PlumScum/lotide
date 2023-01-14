1aef3df66617dfc35a9d1fb27b20b0cbb9ca4461
Jelani Sanders <77513268+PlumScum@users.noreply.github.com>
2023-01-14 03:08:04 -0500

    Refactored, added pseudocode for proper execution, added data validation, cleaned up placeholders

--- countOnly.js
+++ countOnly.js
@@ -1,18 +1,39 @@
-const assertEqual = require('./assertEqual');
-
 const countOnly = function(allItems, itemsToCount) {
+  // If a parameter is empty, return a message and exit the function
+  if (!allItems || !itemsToCount) {
+    console.log("Please enter a valid input. Example: (array of strings, what to count)");
+    return;
+  }
+
+
+  
   const results = {};
 
+  // Loop through each item in the array. I'm using the for loop because it's what I've learned at the given moment.
   for (const item of allItems) {
     if (itemsToCount[item]) {
       if (results[item]) {
-        results[item] += 1;
+        results[item] += 1; //If key exists, increment count by 1
       } else {
-        results[item] = 1;
+        results[item] = 1; //If key does not exist  yet, create key
       }
     }
   }
   return results;
 };
+
+// const firstNames = [
+//   "Karl",
+//   "Salima",
+//   "Agouhanna",
+//   "Fang",
+//   "Kavith",
+//   "Jason",
+//   "Salima",
+//   "Fang",
+//   "Joe"
+// ];
+
+// countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
 module.exports = countOnly;
