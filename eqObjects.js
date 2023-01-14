9ec4ea7fff11fe95b94c7002935d93c91beeb495
Jelani Sanders <77513268+PlumScum@users.noreply.github.com>
2023-01-14 03:39:41 -0500

    Refactored, added pseudocode for proper execution, data validation, cleaned up placeholders

--- eqObjects.js
+++ eqObjects.js
@@ -1,12 +1,25 @@
-const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays.js');
 
-const eqObjects = function(object1, object2) {
+//Compares object keys and values
+const eqObjects = function(object1 = null, object2 = null) {
+  // If a parameter is empty, return a message and exit the function
+  if (!object1 || !object2) {
+    console.log("Error: No objects to compare");
+    return;
+  }
+  
+
+  // Declare our object keys
   const object1Keys = Object.keys(object1);
   const object2Keys = Object.keys(object2);
+
+  // If both objects dont equal the same length, return false
   if (object1Keys.length !== object2Keys.length) {
     return false;
   }
+
+  // We loop our keys against both objects to make sure they are arrays. Then return false if they do not match.
+  // Otherwise return true.
   for (const key of object1Keys) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       return eqArrays(object1[key], object2[key]);
@@ -16,7 +29,5 @@ const eqObjects = function(object1, object2) {
   }
   return true;
 };
-
-
 
 module.exports = eqObjects;
