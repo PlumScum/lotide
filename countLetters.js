77d3ce78f08eb87b9bc6c30cafccf293961cbbe3
Jelani Sanders <jelanisands@gmail.com>
2023-01-14 01:42:32 -0500

    Refactored, added pseudocode for proper execution, added data validation, cleaned up placeholders

--- countLetters.js
+++ countLetters.js
@@ -1,17 +1,25 @@
-const assertEqual = require('./assertEqual');
-
-
 const countLetters = function(sentence) {
+  // If a parameter is empty, return a message and exit the function
+  if (!sentence) {
+    console.log("Please enter a sentence");
+    return;
+  }
+
+  //Declare a object and loop through the sentence character by character, saving each character and returning a corresponding count of characters
   let sentenceObject = {};
   for (const character of sentence) {
     if (sentenceObject[character]) {
-      sentenceObject[character] += 1;
+      sentenceObject[character] += 1; //If key exists, increment count by 1
     } else {
-      sentenceObject[character] = 1;
+      sentenceObject[character] = 1; //If key does not exist  yet, create key
     }
-    delete sentenceObject[" "];
   }
+  delete sentenceObject[" "]; //Remove spaces from object
   return sentenceObject;
 };
 
-console.log(countLetters("Let our house light the way!"));
+
+
+// console.log(countLetters("Let our house light the way!"));
+
+module.exports = countLetters;
