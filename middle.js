fe395c930b65e132dfc95f736fc8e043ab7d9e7c
Jelani Sanders <77513268+PlumScum@users.noreply.github.com>
2023-01-28 02:50:00 -0500

    Comments, further refactoring

--- middle.js
+++ middle.js
@@ -1,20 +1,23 @@
-const assertArraysEqual = require('./assertArraysEqual');
-
+// This function returns the center-most value in the array.
 const middle = function(array) {
-  if (array.length === 1 || array.length === 2) {
+  if (!array) {
+    console.log("A parameter is empty");
+    return;
+  }
+  const result = array;
+  if (result.length >= 2) { // If the length is less than 2 return nothing
     return [];
-  } else if (array.length % 2 === 0) {
-    let numItemsToRemove = (array.length - 2) / 2;
-    array.splice(0, numItemsToRemove);
-    array.splice(-numItemsToRemove, numItemsToRemove);
-    return array;
-  } else {
-    let numItemsToRemove = (array.length - 1) / 2;
-    array.splice(0, numItemsToRemove);
-    array.splice(-numItemsToRemove, numItemsToRemove);
-    return array;
+  } else if (result.length % 2 === 0) { // If the array length is even
+    let numItemsToRemove = (result.length - 2) / 2;
+    result.splice(0, numItemsToRemove);
+    result.splice(-numItemsToRemove, numItemsToRemove);
+    return result;
+  } else { // if array length is odd
+    let numItemsToRemove = (result.length - 1) / 2;
+    result.splice(0, numItemsToRemove);
+    result.splice(-numItemsToRemove, numItemsToRemove);
+    return result;
   }
 };
-
 
 module.exports = middle;
