c3d309bc76db33ea379c26a45e7ef0c76900a3aa
Jelani Sanders <77513268+PlumScum@users.noreply.github.com>
2023-01-28 02:25:44 -0500

    Deconstructed to explain processes

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,5 +1,15 @@
+// This function finds a key by value. It requires an object and value to search for.
 const findKeyByValue = function(object, value) {
-  return Object.keys(object).find(key => object[key] === value);
+  if (!object || !value) {
+    console.log("A parameter is empty");
+    return;
+  }
+  // Loop through each key and check to see if the value is found. If found, return the key.
+  for (let key in object) {
+    if ((object[key]) === value) {
+      return key;
+    }
+  }
 };
 
 module.exports = findKeyByValue;
