060998f4560154f2bae55ba12007e78ecd38d31d
Jelani Sanders <77513268+PlumScum@users.noreply.github.com>
2023-01-28 01:56:39 -0500

    Removed dead code, better comments

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,13 @@
-//compare lengths of two arrays and returns true or false
+// Takes in two arrays and compares them. Returns true or false.
 const eqArrays = function(array1, array2) {
-  // If a parameter is null, blank, return a message and exit the function
+  // If a parameter is empty, return a message and exit the function
   if (!array1 || !array2) {
-    console.log("Error: One or more arrays are invalid.");
-    return false;
+    return "A parameter is missing or empty";
   }
-  
-  //compare lengths of two arrays and returns true or false
   if (array1.length !== array2.length) {
     return false;
   }
+  // We loop through our first array and match it with the second using indices against the arrays.
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
@@ -17,7 +15,5 @@ const eqArrays = function(array1, array2) {
   }
   return true;
 };
-
-// console.log(eqArrays([1, 2, 3], [1, 2, 4]));
 
 module.exports = eqArrays;
