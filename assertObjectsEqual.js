cef5499156c54efa82e7795ba33713240725c756
Jelani Sanders <77513268+PlumScum@users.noreply.github.com>
2023-01-28 01:13:49 -0500

    Further refactoring, better comments

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,21 +1,17 @@
 const eqObjects = require('./eqObjects');
 
-// Using .inspect to easily turn an object into a string
+// This helps with inspecting an object and keeps the code short.
 const inspect = require('util').inspect;
 
+// This asserts whether an object is equal or not.
 const assertObjectsEqual = function(actual, expected) {
   // If a parameter is empty, return a message and exit the function
   if (!actual || !expected) {
-    console.log('Please provide an actual and expected parameter. Example: assertObjectsEqual(actual, expected)');
+    console.log("Parameter is empty");
     return;
   }
-
-  // If the actual and expected objects are/are not equal, output a message
   const message = eqObjects(actual,expected) ? `✅✅✅ Assertion Passed [${inspect(actual)}] === [${inspect(expected)}]` : `🛑🛑🛑 Assertion Failed [${inspect(actual)}] !== [${inspect(expected)}]`;
   console.log(message);
 };
-
-// assertObjectsEqual("Row, row, row your boat", "Row, row, row your boat");
-
 
 module.exports = assertObjectsEqual;
