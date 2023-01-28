2c66723e27e1da8c52a049aa83048cef68fe9df6
Jelani Sanders <77513268+PlumScum@users.noreply.github.com>
2023-01-28 02:00:33 -0500

    Better comments

--- eqObjects.js
+++ eqObjects.js
@@ -1,13 +1,11 @@
 const eqArrays = require('./eqArrays.js');
 
-//Compares object keys and values
-const eqObjects = function(object1 = null, object2 = null) {
+// Takes two objects and compares them
+const eqObjects = function(object1, object2) {
   // If a parameter is empty, return a message and exit the function
   if (!object1 || !object2) {
-    console.log("Error: No objects to compare");
-    return;
+    return "Parameter is empty";
   }
-  
 
   // Declare our object keys
   const object1Keys = Object.keys(object1);
@@ -18,11 +16,15 @@ const eqObjects = function(object1 = null, object2 = null) {
     return false;
   }
 
-  // We loop our keys against both objects to make sure they are arrays. Then return false if they do not match.
-  // Otherwise return true.
   for (const key of object1Keys) {
+    // We loop our keys against both objects to make sure they are arrays. Then return false if they do not match.
+    // Otherwise return true.
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      return eqArrays(object1[key], object2[key]);
+      // If the arrays do not match return false.
+      // Without this statement, it will still return true because they pass as arrays.
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
     } else if (object1[key] !== object2[key]) {
       return false;
     }
