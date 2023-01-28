ae0b975a83589620df895202cdcb48cb4ea11e94
Jelani Sanders <77513268+PlumScum@users.noreply.github.com>
2023-01-28 01:48:31 -0500

    Removed dead code

--- countOnly.js
+++ countOnly.js
@@ -22,18 +22,5 @@ const countOnly = function(allItems, itemsToCount) {
   return results;
 };
 
-// const firstNames = [
-//   "Karl",
-//   "Salima",
-//   "Agouhanna",
-//   "Fang",
-//   "Kavith",
-//   "Jason",
-//   "Salima",
-//   "Fang",
-//   "Joe"
-// ];
-
-// countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
 
 module.exports = countOnly;
