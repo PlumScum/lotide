6676dae66038d77098002260d6e67299b60ae6a5
Jelani Sanders <77513268+PlumScum@users.noreply.github.com>
2023-01-28 02:33:14 -0500

    Deconstructed to explain processes/comments

--- flatten.js
+++ flatten.js
@@ -1,8 +1,25 @@
-const flatten = function(nestedArray) {
-  return nestedArray.flat();
+// This function loops through an array and flattens it if it finds nested arrays
+const flatten = function(array) {
+  if (!array) {
+    console.log("A parameter is empty");
+    return;
+  }
+
+  const flatarray = [];
+
+
+  for (arr of array) {
+    if (Array.isArray(arr)) { // We loop through, if it finds an array we loop and push it to the flat array
+      for (nestedarr of arr) {
+        flatarray.push(nestedarr);
+      }
+    } else { // Otherwise, we push the value to the flat array
+      flatarray.push(arr);
+    }
+  }
+
+  return flatarray;
 };
 
-
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
 
 module.exports = flatten;
