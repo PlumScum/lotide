fb406e0390c7c53d2e6b55e202d0614032d1aa8a
Jelani Sanders <77513268+PlumScum@users.noreply.github.com>
2023-01-28 02:18:28 -0500

    Expanded functionality/corrections from code review

--- findKey.js
+++ findKey.js
@@ -1,14 +1,25 @@
-const assertEqual = require("./assertEqual");
-
+// This function takes in an object and callback and returns the first key found from the callback
 const findKey = function(object, callback) {
-  return callback(object);
+  if (!object || !callback) {
+    console.log("A parameter is empty");
+    return;
+  }
+  
+  for (let key in object) {
+    if (callback(object[key]))
+      return key;
+  }
 };
 
-assertEqual(findKey({
+module.exports = findKey;
+
+/* Example
+  console.log((findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma");
+}, x => x.stars === 3)));
+*/
