20d1d519a5633ed3783df731d0b3e6cace76537f
Jelani Sanders <77513268+PlumScum@users.noreply.github.com>
2023-02-02 00:38:51 -0500

    Replace "strictEqual" with "deepEqual"

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -4,15 +4,15 @@ const eqArrays = require('../eqArrays');
 
 describe("#eqArrays", () => {
   it("returns false when the array lengths aren't the same", () => {
-    assert.strictEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false);
+    assert.deepEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false);
   });
 
   it("returns false when the array lengths ARE the same, but don't match", () => {
-    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+    assert.deepEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
   });
 
   it("returns true when the arrays match", () => {
-    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
   });
 });
 
